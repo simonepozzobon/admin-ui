e811d20d166eb9c25b3e1b410f9ab3b879ea1c85
Angus Simons <info@simonepozzobon.com>
2019-08-06 04:30:06 +0200

    Aggiunge layout classico

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import ClassicLayout from './ClassicLayout.vue'
-import Container from './Container.vue'
-
-export {
-    ClassicLayout,
-    Container,
-}
--- src/install.js
+++ src/install.js
@@ -1,17 +1,1 @@
-import * as components from './components'
 
-const install = (Vue, options = {}) => {
-    for (let key in components) {
-        let _key = options.prefix ? options.prefix + key : key
-        Vue.component(_key, components[key])
-    }
-}
-
-// auto install
-if (typeof window !== 'undefined' && window.Vue) {
-    install(window.Vue)
-}
-
-export {
-    install
-}
